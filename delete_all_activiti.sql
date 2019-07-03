9bb0512043154944a61fd4abb93fc1b4de9caa1a
ruoqi.zhou <1851404291@qq.com>
2019-07-03 14:54:37 +0800

    新增流程sql文件

---
+++ delete_all_activiti.sql
@@ -0,0 +1,34 @@
+DELETE FROM act_cus_basevo;
+DELETE FROM act_cus_user_task;
+
+DELETE FROM act_evt_log;
+DELETE FROM act_hi_actinst;
+DELETE FROM act_hi_attachment;
+DELETE FROM act_hi_comment;
+DELETE FROM act_hi_detail;
+DELETE FROM act_hi_identitylink;
+DELETE FROM act_hi_procinst;
+DELETE FROM act_hi_taskinst;
+DELETE FROM act_hi_varinst;
+-- DELETE FROM act_id_group;
+-- DELETE FROM act_id_info;
+-- DELETE FROM act_id_membership;
+-- DELETE FROM act_id_user;
+DELETE FROM act_procdef_info;
+DELETE FROM act_ru_event_subscr;
+DELETE FROM act_ru_identitylink;
+DELETE FROM act_ru_job;
+DELETE FROM act_ru_task;
+DELETE FROM act_ru_variable;
+
+
+DELETE FROM act_ge_property;
+DELETE FROM act_re_model;
+DELETE FROM act_ge_bytearray;
+DELETE FROM act_re_deployment;
+DELETE FROM act_ru_execution;
+DELETE FROM act_re_procdef;
+
+
+DELETE FROM mis_weeklyreport_project;
+DELETE FROM mis_weeklyreport;
---
+++ drop_activiti_table.sql
@@ -0,0 +1,33 @@
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `act_evt_log`;
+DROP TABLE IF EXISTS `act_hi_actinst`;
+DROP TABLE IF EXISTS `act_hi_attachment`;
+DROP TABLE IF EXISTS `act_hi_comment`;
+DROP TABLE IF EXISTS `act_hi_detail`;
+DROP TABLE IF EXISTS `act_hi_identitylink`;
+DROP TABLE IF EXISTS `act_hi_procinst`;
+DROP TABLE IF EXISTS `act_hi_taskinst`;
+DROP TABLE IF EXISTS `act_hi_varinst`;
+DROP TABLE IF EXISTS `act_id_group`;
+DROP TABLE IF EXISTS `act_id_info`;
+DROP TABLE IF EXISTS `act_id_membership`;
+DROP TABLE IF EXISTS `act_id_user`;
+DROP TABLE IF EXISTS `act_procdef_info`;
+DROP TABLE IF EXISTS `act_re_deployment`;
+DROP TABLE IF EXISTS `act_ru_event_subscr`;
+DROP TABLE IF EXISTS `act_ru_execution`;
+DROP TABLE IF EXISTS `act_ru_identitylink`;
+DROP TABLE IF EXISTS `act_ru_job`;
+DROP TABLE IF EXISTS `act_ru_task`;
+DROP TABLE IF EXISTS `act_ru_variable`;
+
+DROP TABLE IF EXISTS `act_re_procdef`;
+
+DROP TABLE IF EXISTS `act_ge_bytearray`;
+DROP TABLE IF EXISTS `act_ge_property`;
+DROP TABLE IF EXISTS `act_re_model`;
+
+DROP TABLE IF EXISTS `act_cus_basevo`;
+DROP TABLE IF EXISTS `act_cus_user_task`;
